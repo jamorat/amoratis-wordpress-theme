ef94c4302580d553a37eab73746b5e0ba716d053
EC2 Default User <jackamoratis@gmail.com>
2014-09-13 23:12:32 +0000

    start to properly use bootstrap columns

--- index.php
+++ index.php
@@ -1,13 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <script type='text/javascript' src='http://code.jquery.com/jquery-1.7.1.js'></script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap 101 Template</title>
 
+   <script type='text/javascript'>//<![CDATA[ 
+$(window).load(function(){
+
+$(function(){
+    $('#select').click(function(){
+        $('ul').show(); 
+
+
+    });
+    $('.page_item a').click(function(e){
+         $('#select').text($(this).text());
+         $('ul').hide(); 
+        $(this).addClass('current');
+        e.preventDefault();
+    })
+})
+});//]]> 
+</script>
     <!-- Bootstrap -->
-    <link href="wp-content/themes/amoratis-wordpress-theme/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="wp-content/themes/amoratis-wordpress-theme/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+    <link href="wp-content/themes/amoratis-wordpress-theme/jack.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -17,21 +37,35 @@
     <![endif]-->
   </head>
   <body>
+    
+    <h1><?php bloginfo('name'); ?></h1>
+    description <?php bloginfo('description'); ?>
+    url <?php bloginfo('url'); ?>
     <div class="row">
-      <div class="col-xs-12 col-sm-6 col-md-8">.col-xs-12 .col-sm-6 .col-md-8></div>
-      <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-    </div>
-    <div class="row">
-      <div class="col-xs-6 col-sm-4">.col-xs-6 .col-sm-4</div>
-      <div class="col-xs-6 col-sm-4">.col-xs-6 .col-sm-4</div>
-      <!-- Optional: clear the XS cols if their content doesn't match in height -->
-      <div class="clearfix visible-xs-block"></div>
-      <div class="col-xs-6 col-sm-4">.col-xs-6 .col-sm-4</div>
+      <div class="col-md-3 amo_menu">
+        <?php wp_nav_menu( array("container" => "nav", "container_class" => "amo_container", 'fallback_cb'     => 'header_menu','echo' => true)); ?>
+      </div>
+      <div class="col-md-9">
+          <?php 
+          if ( have_posts() ) {
+            while ( have_posts() ) {
+              the_post(); 
+              //
+              // Post Content here ?>
+              <div class="entry">
+              <?php the_content(); ?>
+            </div> <?php
+              //
+            } // end while
+          } // end if
+          ?>
+      </div>
     </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="wp-content/themes/amoratis-wordpress-theme/bootstrap/dist/js/bootstrap.min.js"></script>
+    
   </body>
 </html>
