83b0b3b3662891b25ed754ec909b87ed85a37ea8
EC2 Default User <jackamoratis@gmail.com>
2014-09-14 20:01:40 +0000

    break out code into footer

---
+++ footer.php
@@ -0,0 +1,14 @@
+</div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?php bloginfo( 'template_url' ); ?>/bootstrap/dist/js/bootstrap.js"></script>
+    <script src="<?php bloginfo( 'template_url' ); ?>/bootflat.github.io/bootflat/js/icheck.min.js"></script>
+    <script src="<?php bloginfo( 'template_url' ); ?>/bootflat.github.io/bootflat/js/jquery.fs.selecter.min.js"></script>
+    <script src="<?php bloginfo( 'template_url' ); ?>/bootflat.github.io/bootflat/js/jquery.fs.stepper.min.js"></script>
+    This is an open source web theme created by Jack Amoratis. Fork on GitHub.
+    <br>Or click here to download and use it on your own WordPress site.
+  </body>
+</html>
