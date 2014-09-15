0b350c37dc470c862679e5025cab83adca786f1e
EC2 Default User <jackamoratis@gmail.com>
2014-09-15 23:58:22 +0000

    initial commit of search results page

---
+++ search.php
@@ -0,0 +1,20 @@
+<?php 
+require("header.php");
+global $query_string;
+
+$query_args = explode("&", $query_string);
+$search_query = array();
+
+foreach($query_args as $key => $string) {
+	$query_split = explode("=", $string);
+	$search_query[$query_split[0]] = urldecode($query_split[1]);
+} // foreach
+
+$search = new WP_Query($search_query);
+?>
+
+<?php
+global $wp_query;
+$total_results = $wp_query->found_posts;
+require("footer.php");
+          ?>
