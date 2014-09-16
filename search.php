999e90d7d3ef6735ca2573a0f2d6f7ac19952184
EC2 Default User <jackamoratis@gmail.com>
2014-09-16 02:31:08 +0000

    add search results page to theme

--- search.php
+++ search.php
@@ -1,20 +1,21 @@
 <?php 
 require("header.php");
-global $query_string;
+          
 
-$query_args = explode("&", $query_string);
-$search_query = array();
-
-foreach($query_args as $key => $string) {
-	$query_split = explode("=", $string);
-	$search_query[$query_split[0]] = urldecode($query_split[1]);
-} // foreach
-
-$search = new WP_Query($search_query);
+if ( have_posts() ) : while ( have_posts() ) : the_post();
 ?>
+	<div class="entry">
+		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+ 		<?php the_excerpt(); ?>
+ 	</div><?php
 
-<?php
-global $wp_query;
-$total_results = $wp_query->found_posts;
+endwhile; else : ?>
+	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif;
+
+
 require("footer.php");
-          ?>
+          
+
+?>
+      
