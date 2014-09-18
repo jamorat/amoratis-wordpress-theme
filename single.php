dab9dc9b9aacf2b548b3dfc74a84530652aaf95c
EC2 Default User <jackamoratis@gmail.com>
2014-09-18 03:23:16 +0000

    add title and comment form

--- single.php
+++ single.php
@@ -6,10 +6,16 @@ require("header.php");
               //
               // Post Content here ?>
               <div class="entry">
+                <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
               <?php the_content(); ?>
             </div> <?php
               //
             } // end while
           } // end if
+          ?><div class="comment list">
+    <?php wp_list_comments( array( 'style' => 'div' ) ); ?>dsssss
+</div> HERE IS THE FOOTER
+
+<?php
 require("footer.php");
           ?>
