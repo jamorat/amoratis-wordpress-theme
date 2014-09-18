edb509265eb007276cd62b273881906af9664134
EC2 Default User <jackamoratis@gmail.com>
2014-09-18 01:14:08 +0000

    add title to page posts

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php 
-require("header.php");
+require("header.php"); 
           if ( have_posts() ) {
             while ( have_posts() ) {
               the_post(); 
               //
               // Post Content here ?>
               <div class="entry">
+                <h1><?php the_title(); ?></h1>
               <?php the_content(); ?>
             </div> <?php
               //
