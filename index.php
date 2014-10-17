79127ae6e2fde38429efd180988a45cab701042c
EC2 Default User <jackamoratis@gmail.com>
2014-10-17 00:59:28 +0000

    add date to post listing

--- index.php
+++ index.php
@@ -7,10 +7,11 @@
               <div class="entry">
                 <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
               <?php the_content(); ?>
+              Date Posted: <?php the_date(); ?>
             </div> <?php
               //
             } // end while
           } // end if
-get_footer();
+      get_footer();
           ?>
       
